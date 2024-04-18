d3e6c2404505f79ef5d52b3d407c7035b999c318
Pallinojr <149068528+Pallinojr@users.noreply.github.com>
2024-04-18 13:19:35 +0200

    Add files via upload

--- RLE.java
+++ RLE.java
@@ -19,10 +19,10 @@ public class RLE extends Application {
 	TextField tImgNoCompressa = new TextField("");
 	TextField tImgCompressa = new TextField("");
 	TextField tCompressionRate = new TextField("");
-	
+
 	char immagine[][]=new char[3][3];
-	
-	Hashtable<Integer, String> my_dizionario = new Hashtable<Integer,String>();
+
+	Hashtable<Integer, Integer> my_dizionario = new Hashtable<Integer,Integer>();
 
 
 
@@ -37,13 +37,21 @@ public class RLE extends Application {
 		griglia.add(tImgNoCompressa, 1,0);
 		griglia.add(tImgCompressa, 1,1);
 		griglia.add(tCompressionRate, 1,2);
-		
-		my_dizionario.put(10,"A");
-		my_dizionario.put(11,"B");
-		my_dizionario.put(12,"C");
-		my_dizionario.put(13,"D");
-		my_dizionario.put(14,"E");
-		my_dizionario.put(15,"F");
+
+		my_dizionario.put(1,1);
+		my_dizionario.put(2,2);
+		my_dizionario.put(3,2);
+		my_dizionario.put(4,3);
+
+		immagine[0][0]='V';
+		immagine[0][1]='B';
+		immagine[0][2]='B';
+		immagine[1][0]='G';
+		immagine[1][1]='G';
+		immagine[1][2]='G';
+		immagine[2][0]='B';
+		immagine[2][1]='V';
+		immagine[2][2]='N';
 
 
 		bCalcola.setOnAction(e ->calcola());
@@ -57,18 +65,31 @@ public class RLE extends Application {
 		finestra.show();		
 	}
 	public void calcola() {
-		int nCompresso=0;
-		char color=' ' ;
-		for(int i=0;i<immagine.length;i++) {
-			for(int j=1;j<immagine.length;j++) {
-				if(j==0) {
-					color=immagine[i][j];
-				}
-				if(j==0) {
-					color=immagine[i][j];
-				}
+		int bit=0;
+		int compresso=0;
+		int noCompresso=24*9;
+		double rate=0;
+		for(int ix=0;ix<3;ix++) {
+			for(int iy=0;iy<3;iy++) {
+				bit+=24;	
 			}
 		}
+		for(int ix=0;ix<3;ix++) {
+			for(int iy=1;iy<3;iy++) {
+				if(immagine[ix][iy]==(immagine[ix][iy-1])) {
+					compresso+=my_dizionario.get(2);
+				}else {
+					compresso=compresso+24+my_dizionario.get(1);
+				}
+			}
+			compresso+=24;
+		}
+		rate=100-(compresso/(double)noCompresso*100);
+		compresso=compresso-1;
+		tImgNoCompressa.setText(bit+"");
+		tImgCompressa.setText(compresso+"");
+		tCompressionRate.setText(rate+"");
+
 	}
 	public static void main(String[] args) {
 		launch(args);
